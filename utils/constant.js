6ece5a2bdce7357d123bea332a01c453c9ef2e68
amitkumar18596 <maharana.amit96@gmail.com>
2022-08-23 08:21:12 +0530

    created ticket model

--- utils/constant.js
+++ utils/constant.js
@@ -12,5 +12,10 @@ module.exports = {
         pending : "PENDING",
         approved : "APPROVED",
         rejected : "REJECTED"
+    },
+    ticketStatus : {
+        open : "OPEN",
+        closed : "CLOSED",
+        blocked : "BLOCKED"
     }
 }
